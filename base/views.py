cec0f533dc563cc48a3bfdba3c5d3744dacb4355
1 <a1@1s-MacBook-Air.local>
2024-04-02 18:40:09 +0100

    project code push

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-#comment
+#commentbgit a
 
 @login_required
 def home(request):
